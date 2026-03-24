61d38e8d6e427ec05f06adad31e863d1fb89ae4e
muskanmp <muskanmp2000@gmail.com>
2026-03-24 14:27:42 +0530

    Initial commit of all Microservices

---
+++ limits-services/src/main/java/com/limitsmicroservices/limits_services/bean/Limits.java
@@ -0,0 +1,28 @@
+package com.limitsmicroservices.limits_services.bean;
+
+public class Limits{
+    private int minimun;
+    private int maximum;
+    
+    public Limits() {
+        super();
+    }
+    public Limits(int minimun, int maximum) {
+        this.minimun = minimun;
+        this.maximum = maximum;
+    }
+    public int getMinimun() {
+        return minimun;
+    }
+    public void setMinimun(int minimun) {
+        this.minimun = minimun;
+    }
+    public int getMaximum() {
+        return maximum;
+    }
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    
+}
---
+++ limits-services/src/main/java/com/limitsmicroservices/limits_services/controller/LimitsController.java
@@ -0,0 +1,21 @@
+package com.limitsmicroservices.limits_services.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.limitsmicroservices.limits_services.bean.Limits;
+import com.limitsmicroservices.limits_services.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+   
+    @Autowired
+    private Configuration configuration;
+    
+    @GetMapping("/limits")
+    public Limits getLimits(){
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+        // return new Limits(1,1000);
+    }
+}
